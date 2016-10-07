09789d436b5e9e9c121aae48fd6e5eb32d4ea92d
Jos Nienhuis <jos_nienhuis@hotmail.com>
2016-10-07 15:19:28 +0200

    Added help button in footer

---
+++ Utilities/HtmlUtility.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Web.Hosting;
+
+namespace HyperVAdmin.Utilities
+{
+    /// <summary>
+    /// A utility class to help with HTML related tasks.
+    /// </summary>
+    public static class HtmlUtility
+    {
+        /// <summary>
+        /// Retrieves the VirtualDirectory/Path (also for "IIS Applications") when not hosted in the root of the domain
+        /// </summary>
+        /// <returns>Either the VirtualDirectory/Path (with trailing slash) or "/" when hosted in the domain</returns>
+        public static string GetVirtualApplicationPath()
+        {
+            string path = HostingEnvironment.ApplicationVirtualPath;
+
+            if (path.Substring(path.Length - 1) != "/")
+            {
+                path += "/";
+            }
+
+            return path;
+        }
+    }
+}
