d7148be578a380be26de760970677dfb3c6598b3
Jos Nienhuis <jos_nienhuis@hotmail.com>
2017-04-17 09:01:26 +0200

    Remove bat script

--- build_documentation.bat
+++
@@ -1,2 +0,0 @@
-docfx Documentation\docfx.json
-yuidoc --configfile C:\Users\jos_n\Documents\GitHub\HyperVAdmin\Content\Scripts\yuidoc.json C:\Users\jos_n\Documents\GitHub\HyperVAdmin\Content\Scripts\
