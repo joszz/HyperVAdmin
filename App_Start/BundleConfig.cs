fdb90f46aa07448c8cb4bf1a7c8c042f552e0aab
Jos Nienhuis <jos_nienhuis@hotmail.com>
2020-08-10 17:18:42 +0200

    Update libman

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -11,17 +11,17 @@ namespace HyperVAdmin
                 "~/Content/Vendor/jquery/jquery.js",
                 "~/Content/Vendor/fancybox/jquery.fancybox.js",
                 "~/Content/Vendor/jquery-fullscreen-plugin/jquery.fullscreen.js",
+                "~/Content/Vendor/jquery.isloading/jquery.isloading.js",
+                "~/Content/Vendor/jquery.vibrate/jquery.vibrate.js",
+                "~/Content/Vendor/waves/waves.js",
+                "~/Content/Vendor/sorttable/sorttable.js",
 
-                "~/Content/Scripts/jquery.isloading.js",
-                "~/Content/Scripts/jquery.vibrate.js",
-                "~/Content/Scripts/sorttable.js",
-                "~/Content/Scripts/Waves/waves.js",
                 "~/Content/Scripts/general.js"
             ));
 
             bundles.Add(new StyleBundle("~/Content/Styles/bundle").Include(
                 "~/Content/Vendor/fancybox/jquery.fancybox.css",
-                "~/Content/Styles/Waves/waves.css",
+                "~/Content/Vendor/waves/waves.css",
                 "~/Content/Styles/Default.css"
             ));
         }
