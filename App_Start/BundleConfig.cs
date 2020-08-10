5d2f875a23c277224a235fc011421e16c0b11e68
Jos Nienhuis <jos_nienhuis@hotmail.com>
2020-08-10 09:13:52 +0200

    WIP Moving thirdparty clientside libraries to libman

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,22 +8,21 @@ namespace HyperVAdmin
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                "~/Content/Scripts/jQuery/jquery-{version}.js",
+                "~/Content/Vendor/jquery/jquery.js",
+                "~/Content/Vendor/fancybox/jquery.fancybox.js",
+                "~/Content/Vendor/jquery-fullscreen-plugin/jquery.fullscreen.js",
+
                 "~/Content/Scripts/Bootstrap/bootstrap.js",
-                "~/Content/Scripts/Fancybox/jquery.fancybox.js",
                 "~/Content/Scripts/jquery.isloading.js",
                 "~/Content/Scripts/jquery.vibrate.js",
-                "~/Content/Scripts/jquery.fullscreen.js",
                 "~/Content/Scripts/sorttable.js",
                 "~/Content/Scripts/Waves/waves.js",
-                "~/Content/Scripts/BootstrapToggle/bootstrap-toggle.js",
                 "~/Content/Scripts/general.js"
             ));
 
             bundles.Add(new StyleBundle("~/Content/Styles/bundle").Include(
-                "~/Content/Styles/Fancybox/jquery.fancybox.css",
+                "~/Content/Vendor/fancybox/jquery.fancybox.css",
                 "~/Content/Styles/Waves/waves.css",
-                "~/Content/Styles/BootstrapToggle/bootstrap-toggle.css",
                 "~/Content/Styles/Default.css"
             ));
         }
